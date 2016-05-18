99a760b9379e27c6c46497a3b90dcad7f2d7d1c2
Jerrol Krause <jkrause@LD.corp.local>
2016-05-18 12:55:46 -0700

    Removed CDNIFY check cuz its slow

--- Gruntfile.js
+++ Gruntfile.js
@@ -510,7 +510,7 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
+    //'cdnify',
     'cssmin',
     'uglify',
     'filerev',
@@ -519,7 +519,7 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('default', [
-    'newer:jshint',
+    //'newer:jshint',
     'newer:jscs',
     'test',
     'build'
